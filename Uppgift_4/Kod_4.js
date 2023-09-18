8bd716e3a4e0299aca815958620c4a54c21b5c7d
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-09-18 12:40:07 +0200

    Fourth code

---
+++ Uppgift_4/Kod_4.js
@@ -0,0 +1,6 @@
+const browserType = "Hello World" + "Hell";
+        if (browserType.includes("Hello") && browserType.startsWith("Hell")) {
+          console.log(true);
+        } else {
+          console.log(false);
+        }
